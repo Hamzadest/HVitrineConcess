e5d7028ab8aeab95c2ed67446e3af05bd5321787
Hamzadest <64666220+Hamzadest@users.noreply.github.com>
2021-01-21 20:47:43 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'bodacious'
+
+game 'gta5'
+
+author 'Hamza#8602'
+description 'Vitrine pour concessionnaire / Showcase for car dealer'
+version '1.0.0'
+
+client_scripts {
+	'client.lua
+}
